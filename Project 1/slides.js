d2107745bafb254671c2e610cfab29ce9b9438ef
AgonLohaj <agon.lohaj@gmail.com>
2015-04-29 22:07:51 +0200

    presentation

--- Project 1/slides.js
+++ Project 1/slides.js
@@ -1,10 +1,16 @@
-;jQuery(document).ready(function($){
-
-	$('.github-widget').each(function(){
-		$container = this;
-		$widget = $("...some html...");
-		$widget.appendTo($container);
-		...
-		// functions to initialise
-	}
-}
+// Refactored Code:
+;(function ( $, window, document, undefined ) {
+	$.widget( "custom.githubWidget" , {
+		options: { ... }, _create: function () {
+			var self = this;
+			self.element.addClass("github-box repo");
+			$widget = $("...some html code...");
+			self.element.append($widget);
+			self.element.bind( "refresh", function(e){
+				if(self.options.repo != null){
+					// get data
+				}
+			});
+		}, destroy: function () { ... }, _setOption: function ( key, value ) { ... },_refreshRepo: function() { ... }, getData: function() { ... },
+	});
+})( jQuery, window, document );
